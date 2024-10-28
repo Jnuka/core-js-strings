c70af409d2e63b1d19908179d71491e7d4ea7150
Jnuka <jnuka@mail.ru>
2024-10-28 16:45:05 +0400

    feat: add isPalindrome()

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -327,8 +327,9 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const replaceStr = str.toLowerCase().replace(/[?,! ]/g, '');
+  return replaceStr === replaceStr.split('').reverse().join('');
 }
 
 /**
